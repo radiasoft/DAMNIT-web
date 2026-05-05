aced21faefbd22d6d4a18501f391d3ac2fe5bcd3
Rob Nagler <5495179+robnagler@users.noreply.github.com>
2026-05-05 23:11:56 +0000

    refactored to be clearer

--- api/src/damnit_api/proposal_api.py
+++ api/src/damnit_api/proposal_api.py
@@ -3,6 +3,9 @@ from pykern.pkdebug import pkdc, pkdlog, pkdp
 import damnit.api
 import damnit_api.data
 import damnit_api.db
+import damnit_api.graphql.models
+import damnit_api.graphql.queries
+import damnit_api.shared.const
 import pykern.pkconfig
 import pykern.quest
 
@@ -15,19 +18,28 @@ _cfg = pykern.pkconfig.init(
 class API(pykern.quest.API):
 
     async def api_table(self, api_args):
-        from damnit_api.graphql.queries import fetch_variables
+        def _convert(row):
+            for n, e in row.items():
+                if e is not None:
+                    yield _convert_var(n, e)
 
-        return PKDict(
-            runs=await fetch_variables(
+        def _convert_var(name, entry):
+            t = damnit_api.graphql.models.DamnitRun.get_dtype(name, entry)
+            v = entry.get("value") if isinstance(entry, dict) else entry
+            if t != damnit_api.shared.const.DamnitType.IMAGE:
+                v, t = damnit_api.graphql.models.serialize(v, dtype=t)
+            return PKDict(name=name, value=v, dtype=t.value)
+
+        async def _rows():
+            return await damnit_api.graphql.queries.fetch_variables(
                 api_args.proposal,
                 limit=api_args.runs_per_page,
                 offset=(api_args.start_page - 1) * api_args.runs_per_page,
             )
-        )
+
+        return PKDict(runs=[PKDict(variables=tuple(_convert(x))) for x in await _rows()])
 
     async def api_image(self, api_args):
-        import damnit_api.db
-
         p = damnit_api.db.find_proposal_path(_cfg.damnit_path, api_args.proposal)
         d = damnit.api.Damnit(p)[api_args.run, api_args.variable].read()
         return PKDict(
